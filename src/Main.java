55307ff3769feeb64c218328f19b48e7f2f1163b
Erensar <richard.fitz.gilbert@yandex.ru>
2024-07-12 08:46:07 +0300

    6th corrections 2

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,7 @@
-import manager.InMemoryTaskManager;
-import manager.Managers;
-import manager.TaskManager;
-import task.Epic;
-import task.Status;
-import task.Subtask;
-import task.Task;
-
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Поехали!");
-        TaskManager taskManager = Managers.getDefault();
-
-
-
     }
 }
